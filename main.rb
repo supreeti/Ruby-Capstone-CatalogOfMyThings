949625846d927260fb0c078b4caa273c0b1ff958
rod <rodrip0129@gmail.com>
2023-09-19 09:39:03 -0300

    create a main file and add the list of options

---
+++ main.rb
@@ -0,0 +1,107 @@
+require 'json'
+
+BOOKS_FILE = 'books.json'
+ALBUMS_FILE = 'albums.json'
+MOVIES_FILE = 'movies.json'
+GAMES_FILE = 'games.json'
+
+
+# def list_books
+  
+# end
+
+# def list_albums
+  
+# end
+
+# def list_movies
+  
+# end
+
+# def list_games
+  
+# end
+
+# def list_genres
+  
+# end
+
+# def list_labels
+  
+# end
+
+# def list_authors
+  
+# end
+
+# def list_sources
+  
+# end
+
+# def add_book
+
+# end
+
+# def add_album
+
+# end
+
+# def add_movie
+
+# end
+
+# def add_game
+
+# end
+
+loop do
+  puts "\nOptions:"
+  puts "1. List all books"
+  puts "2. List all music albums"
+  puts "3. List all movies"
+  puts "4. List all games"
+  puts "5. List all genres"
+  puts "6. List all labels"
+  puts "7. List all authors"
+  puts "8. List all sources"
+  puts "9. Add a book"
+  puts "10. Add a music album"
+  puts "11. Add a movie"
+  puts "12. Add a game"
+  puts "13. Quit"
+
+  print "Choose an option: "
+  choice = gets.chomp.to_i
+
+  case choice
+  when 1
+    list_books
+  when 2
+    list_albums
+  when 3
+    list_movies
+  when 4
+    list_games
+  when 5
+    list_genres
+  when 6
+    list_labels
+  when 7
+    list_authors
+  when 8
+    list_sources
+  when 9
+    add_book
+  when 10
+    add_album
+  when 11
+    add_movie
+  when 12
+    add_game
+  when 13
+    puts "Goodbye!"
+    break
+  else
+    puts "Invalid choice. Please select a valid option."
+  end
+end
