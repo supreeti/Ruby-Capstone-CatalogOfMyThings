56ac8aeaee4045621203f1191e8f2b967cc5e3c1
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-09-20 19:40:30 +0100

    Set up properties of game and author class

---
+++ classes/game.rb
@@ -0,0 +1,16 @@
+require_relative 'item'
+
+class Game < Item
+  def initialize(multilayer, last_played_at, archived, publish_date, author)
+    super(archived, publish_date)
+    @multilayer = multilayer
+    @last_played_at = last_played_at
+    author.add_item(self)
+  end
+
+  private
+
+  def can_be_archived?
+    super and (Time.new.year - @last_played_at.year) > 2
+  end
+end
