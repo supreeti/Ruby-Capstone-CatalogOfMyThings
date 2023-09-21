a35401160c351ae697d9a64249133d607b1023db
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-09-21 11:05:08 +0100

    Update game.rb

--- game.rb
+++ game.rb
@@ -12,6 +12,7 @@ class Game < Item
   private
 
   def can_be_archived?
-    super && (Time.new.year - @last_played_at.year) > 2
+    def can_be_archived?
+        super && last_played_at < (Date.today - 730) # 2 years = 730 days
   end
 end
