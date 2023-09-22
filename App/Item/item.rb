00a1b678b363df80c94f63f6c43b86343b35b1ba
rod <rodrip0129@gmail.com>
2023-09-22 20:50:34 -0300

    add methods on Item to help establish the '1 - many' relationship

--- App/Item/item.rb
+++ App/Item/item.rb
@@ -2,14 +2,14 @@ require 'date'
 
 class Item
   attr_reader :id, :genre, :label, :publish_date
-  attr_accessor :archived, :author
+  attr_accessor :archived
 
   def initialize(genre, author, label, publish_date, archived: false)
     @id = Random.rand(1..1000)
-    @genre = genre
-    @label = label
+    self.genre = genre
+    self.label = label
     @publish_date = publish_date
-    @author = author
+    self.author = author
     @archived = archived
   end
 
@@ -23,5 +23,20 @@ class Item
     else
       puts 'Item cannot be archived...'
     end
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.items.push(self) unless genre.items.include?(self)
+  end
+
+  def label=(label)
+    @label = label
+    label.items.push(self) unless label.items.include?(self)
+  end
+
+  def author=(author)
+    @author = author
+    author.items.push(self) unless author.items.include?(self)
   end
 end
