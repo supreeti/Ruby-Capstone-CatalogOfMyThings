3b21f87eb16fcc6f93df7cae3022dc2840d8a4cc
Supreeti Kushwaha <sksupreetk11@gmail.com>
2023-09-24 00:03:09 +0530

    resolve test error

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,40 +1,84 @@
 require_relative '../App/Item/item'
+require_relative '../App/Genre/genre'
+require_relative '../App/Book/label'
+require_relative '../App/Author/author'
 require 'date'
 
+# Define mock classes for associations
+class Genre
+  attr_accessor :items
+  def initialize
+    @items = []
+  end
+end
+
+class Label
+  attr_accessor :items
+  def initialize
+    @items = []
+  end
+end
+
+class Author
+  attr_accessor :items
+  def initialize
+    @items = []
+  end
+end
+
 describe Item do
-  describe '#initialize' do
-    it 'creates a new item with the provided attributes' do
-      item = Item.new('action', 'martin', 'Ray_publishing', Date.parse('2012-09-21'))
-      expect(item.genre).to eq('action')
-      expect(item.author).to eq('martin')
-      expect(item.label).to eq('Ray_publishing')
-      expect(item.publish_date).to eq(Date.parse('2012-09-21'))
-      expect(item.archived).to be(false)
-    end
+  let(:item) { Item.new }
+
+  it 'has a unique ID' do
+    expect(item.id).to be_between(1, 1000).inclusive
+  end
+
+  it 'is not archived by default' do
+    expect(item.archived).to be false
   end
 
   describe '#can_be_archived?' do
-    it 'returns true if the item is older than 10 years' do
-      item = Item.new('action', 'martin', 'Ray_publishing', Date.parse('2012-09-21'))
-      expect(item.can_be_archived?).to be(true)
+    it 'returns true if the publish date is more than 10 years ago' do
+      item.instance_variable_set(:@publish_date, Date.today - (11 * 365))
+      expect(item.can_be_archived?).to be true
     end
 
-    it 'returns false if the item is not older than 10 years' do
-      item = Item.new('action', 'martin', 'Ray_publishing', Date.parse('2019-09-21'))
-      expect(item.can_be_archived?).to be(false)
+    it 'returns false if the publish date is less than 10 years ago' do
+      item.instance_variable_set(:@publish_date, Date.today - (9 * 365))
+      expect(item.can_be_archived?).to be false
     end
   end
 
   describe '#move_to_archive' do
-    it 'archives item if it can be archived' do
-      item = Item.new('action', 'martin', 'Ray_publishing', Date.parse('2012-09-21'))
-      item.move_to_archive
-      expect(item.archived).to be(true)
+    it 'archives the item if it can be archived' do
+      item.instance_variable_set(:@publish_date, Date.today - (11 * 365))
+      expect { item.move_to_archive }.to change { item.archived }.from(false).to(true)
     end
+
     it 'does not archive the item if it cannot be archived' do
-      item = Item.new('action', 'martin', 'Ray_publishing', Date.parse('2019-09-21'))
-      expect { item.move_to_archive }.to output("Item cannot be archived.\n").to_stdout
-      expect(item.archived).to be(false)
+      item.instance_variable_set(:@publish_date, Date.today - (9 * 365))
+      expect { item.move_to_archive }.not_to change { item.archived }
+    end
+  end
+
+  describe 'associations' do
+    let(:genre) { Genre.new }
+    let(:label) { Label.new }
+    let(:author) { Author.new }
+
+    it 'adds itself to genre items' do
+      item.genre = genre
+      expect(genre.items).to include(item)
+    end
+
+    it 'adds itself to label items' do
+      item.label = label
+      expect(label.items).to include(item)
+    end
+
+    it 'adds itself to author items' do
+      item.author = author
+      expect(author.items).to include(item)
     end
   end
 end
